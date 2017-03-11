9479edba37d9d493ced96006c2d4f048c0cb2e92
bobbyschank <robertschank@gmail.com>
2017-03-11 15:30:34 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
  console.log("Up and running!");
- var cardOne = 'queen';
- var cardTwo = 'king';
- var cardThree = 'queen';
- var cardFour = 'king';
- console.log("user flipped " + cardOne);
- console.log("user flipped " + cardTwo);
+
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("user flipped: " + cardsInPlay[cardsInPlay.length-1]);
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log("user flipped: " + cardsInPlay[cardsInPlay.length-1]);
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert('You found a match!');
+	} else {
+		alert('Sorry, try again.');
+	}
+}
