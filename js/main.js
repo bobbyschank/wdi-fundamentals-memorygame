8e75445362212601348c1432513b5945c56d5dbf
bobbyschank <robertschank@gmail.com>
2017-03-13 01:03:10 -0600

    finished mvp

--- js/main.js
+++ js/main.js
@@ -30,20 +30,31 @@ var checkForMatch = function() {
 	} else {
 		alert('Sorry, try again.');
 	}
-}
+};
 
-var flipCard = function(cardId) {
-
+var flipCard = function() {
+	console.log('data-id' + this.getAttribute('data-id'));
+	var cardId = this.getAttribute('data-id');
 	console.log('User flipped ' + cards[cardId].rank);
 	console.log(cards[cardId].suit);
 	console.log(cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId].rank);
 
+	this.setAttribute('src', cards[cardId].cardImage);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
 
-}
+};
 
-flipCard(0);
-flipCard(1);
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+createBoard();
